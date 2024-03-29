90167fc94c2e0d4e02b6ef79449a40a368ab7f90
Balamathias <balamathias05@gmail.com>
2024-03-29 10:51:09 +0100

    Python requests on the go

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status"""
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        response = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(response)}")
+        print(f"\t- content: {response}")
+        print(f"\t- utf8 content: {response.decode(encoding='utf-8')}")
